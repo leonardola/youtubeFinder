8f18ba751baaca1c1873db1bead04e50ef8070f1
leonardoalbuquerque <leonardo.lopes.albuquerque@gmail.com>
2014-09-06 18:48:53 -0300

    created a condition, making the download only happen if the file videoList.txt is not empty

--- execute.sh
+++ execute.sh
@@ -2,6 +2,14 @@ cd /home/cubie/youtubeFinder
 
 php downloader.php
 
-cd /media/hd/series/youtube
+if [ `ls -l videoList.txt | awk '{print $5}'` -eq 0 ]
+then
+    //empty
+    echo "the file was empty"
+else
+    cd /media/hd/series/youtube
 
-/usr/local/bin/youtube-dl -w -t -a /home/cubie/youtubeFinder/videoList.txt
+    /usr/local/bin/youtube-dl -w -t -a /home/cubie/youtubeFinder/videoList.txt
+fi
+
+
